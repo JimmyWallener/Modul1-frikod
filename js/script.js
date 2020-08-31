e812dafd355a2cf3f0a4b7383f318b226b12a60a
Jimmy Wallener <38855083+jimwdev@users.noreply.github.com>
2020-08-31 09:53:43 +0200

    added self invoked function

--- js/script.js
+++ js/script.js
@@ -13,71 +13,72 @@ Syftet är att öva på användning:
 #appendChild etc
 
 */
+(function () {
+  window.addEventListener("load", () => {
+    loadContent();
+  });
 
-window.addEventListener("load", () => {
-  loadContent();
-});
+  setInterval(() => {
+    changeColor();
+  }, 30000 * 1);
 
-setInterval(() => {
-  changeColor();
-}, 30000 * 1);
+  /*
+  FUNCTIONS HERE
+  */
 
-/*
-FUNCTIONS HERE
-*/
+  // Lame function which changes background color on the divs. Just to use querySelectorAll
+  function changeColor() {
+    const div = document.querySelectorAll("div");
 
-// Lame function which changes background color on the divs. Just to use querySelectorAll
-function changeColor() {
-  const div = document.querySelectorAll("div");
+    console.log(typeof div);
+    console.log(div.length);
 
-  console.log(typeof div);
-  console.log(div.length);
-
-  const index = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += index[Math.floor(Math.random() * 16)];
+    const index = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += index[Math.floor(Math.random() * 16)];
+    }
+    div.forEach((e) => {
+      e.style.backgroundColor = color;
+    });
   }
-  div.forEach((e) => {
-    e.style.backgroundColor = color;
-  });
-}
 
-// Create buttons with urls
-function populateLinks(json) {
-  const linkList = document.querySelector(".links_list");
-  const ul = document.createElement("ul");
+  // Create buttons with urls
+  function populateLinks(json) {
+    const linkList = document.querySelector(".links_list");
+    const ul = document.createElement("ul");
 
-  json.map((links) => {
-    const li = document.createElement("li");
-    const button = document.createElement("button");
-
-    button.innerText = links.link;
-    button.classList.add("link_button");
-    button.classList.add(`${links.id}`);
-    li.appendChild(button);
-    ul.appendChild(li);
-  });
-  linkList.appendChild(ul);
-
-  const click = document.querySelectorAll(".link_button");
-
-  click.forEach((e) => {
     json.map((links) => {
-      let link = links.link;
-      let id = links.id;
-      e.addEventListener("click", () => {
-        if (e.classList.item(1) == id) {
-          location.assign("http://" + link);
-        }
+      const li = document.createElement("li");
+      const button = document.createElement("button");
+
+      button.innerText = links.link;
+      button.classList.add("link_button");
+      button.classList.add(`${links.id}`);
+      li.appendChild(button);
+      ul.appendChild(li);
+    });
+    linkList.appendChild(ul);
+
+    const click = document.querySelectorAll(".link_button");
+
+    click.forEach((e) => {
+      json.map((links) => {
+        let link = links.link;
+        let id = links.id;
+        e.addEventListener("click", () => {
+          if (e.classList.item(1) == id) {
+            location.assign("http://" + link);
+          }
+        });
       });
     });
-  });
-}
-// Getting urls from external json file
-async function loadContent() {
-  const data = fetch("./json/links.json")
-    .then((response) => response.json())
-    .then((json) => populateLinks(json))
-    .catch((err) => console.warn(err));
-}
+  }
+  // Getting urls from external json file
+  async function loadContent() {
+    const data = fetch("./json/links.json")
+      .then((response) => response.json())
+      .then((json) => populateLinks(json))
+      .catch((err) => console.warn(err));
+  }
+})();
